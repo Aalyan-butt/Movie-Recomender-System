028058466e99e39ff5725748ff4e0ef524f82980
Aalyan-butt <aalyanriasat@gmail.com>
2025-01-18 14:34:24 +0500

    .pkl

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
+import pickle
+
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies_list = movies_list['title'].values
+
+
 
 st.title('Movie Recommendor System')
 
 option = st.selectbox(
     "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
+    movies_list
 )
